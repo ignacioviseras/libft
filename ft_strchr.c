7474cc0dee31c8ca526fbf79269a8dc05d4dd143
pc42 <ignacio.viseras@gmail.com>
2023-09-29 21:32:46 +0200

    funciona

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: igvisera <igvisera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 23:06:04 by igvisera          #+#    #+#             */
-/*   Updated: 2023/09/29 21:13:26 by igvisera         ###   ########.fr       */
+/*   Updated: 2023/09/29 21:31:27 by igvisera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	index;
@@ -22,54 +21,32 @@ char	*ft_strchr(const char *s, int c)
 	index = 0;
 	while (s[index] != '\0')
 	{
-		if (s[index] == '\0' || s[index] == c)
-		{
-			return ((char *)s);
-		}
-	}
-	return (NULL);
-}
-*/
-
-/*El	strchr(void) funciones devuelven un puntero al
-carácter coincidente o NULL si no se encuentra el carácter.
-
-los el byte nulo final se considera parte de la cadena,
-de modo que si c se especifica como '\0',
-estas funciones devuelven un puntero a El terminador. */
-char	*ft_strchr(const char *s, int c)
-{
-	size_t	index;
-
-	index = 0;
-	if (c == '\0')
-		return (s[index]);
-	while (s[index] != '\0')
-	{
-		if (s[index] == c)
+		if (s[index] == (char)c)
 			return ((char *)s + index);
 		index++;
 	}
+	if (c == '\0')
+		return ((char *)s + index);
 	return (NULL);
 }
-int	main(void)
-{
-	char s[] = "tripouille";
+// int	main(void)
+// {
+// 	char s[] = "tripouille";
 
-	printf("%s\n", ft_strchr(s, 't'));
-	printf("%s\n", strchr(s, 't'));
-	printf("%s\n", "--------------------------");
-	printf("%s\n", ft_strchr(s, 'l'));
-	printf("%s\n", strchr(s, 'l'));
-	printf("%s\n", "--------------------------");
-	printf("%s\n", ft_strchr(s, 'z'));
-	printf("%s\n", strchr(s, 'z'));
-	printf("%s\n", "--------------------------");
-	printf("%s\n", ft_strchr(s, 0));
-	printf("%s\n", strchr(s, 0));
-	printf("%s\n", "--------------------------");
-	printf("%s\n", ft_strchr(s, 't' + 256));
-	printf("%s\n", strchr(s, 't' + 256));
-	printf("%s\n", "--------------------------");
+// 	printf("%s\n", ft_strchr(s, 't'));
+// 	printf("%s\n", strchr(s, 't'));
+// 	printf("%s\n", "--------------------------");
+// 	printf("%s\n", ft_strchr(s, 'l'));
+// 	printf("%s\n", strchr(s, 'l'));
+// 	printf("%s\n", "--------------------------");
+// 	printf("%s\n", ft_strchr(s, 'z'));
+// 	printf("%s\n", strchr(s, 'z'));
+// 	printf("%s\n", "--------------------------");
+// 	printf("%s\n", ft_strchr(s, 0));
+// 	printf("%s\n", strchr(s, 0));
+// 	printf("%s\n", "--------------------------");
+// 	printf("%s\n", ft_strchr(s, 't' + 256));
+// 	printf("%s\n", strchr(s, 't' + 256));
+// 	printf("%s\n", "--------------------------");
 
-}
+// }
